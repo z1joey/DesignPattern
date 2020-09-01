23b34aef05f9b72c09096dd61f55ed0401216361
Joey Zhang <joey.zhang@tgitech.net>
2020-09-02 00:41:36 +0800

    refactor: demo

--- SOLID/SOLID_Demo.swift
+++ SOLID/SOLID_Demo.swift
@@ -1,55 +1,75 @@
-public protocol TimerOption {
+public protocol Option {
+    var index: Int { get set }
+    var description: String { get }
+}
+
+public protocol TimerOption: Option {
     var hours: Int { get set }
     var minutes: Int { get set }
     var seconds: Int { get set }
 }
 
-public protocol TemperatureOption {
+public protocol TemperatureOption: Option {
     var temperature: Float { get set }
 }
 
-public protocol MotorOption {
+public protocol SpeedOption: Option {
     var speed: Int { get set }
-    var isClockwise: Bool { get set }
 }
 
-public protocol HeatOption {
+public protocol ReversableOption: Option {
+    var isReverse: Bool { get set }
+}
+
+public protocol HeatOption: Option {
     var heatAt: Int { get set }
 }
 
-public protocol FanOption {
+public protocol FanOption: Option {
     var isFanOn: Bool { get set }
 }
 
-public protocol ScaleOption {
+public protocol ScaleOption: Option {
     var weight: Float { get set }
 }
 
-public protocol DeviceMode {
-    static var cellIndexes: [Int] { get set }
+public class Fan: FanOption {
+    public var index: Int = 0
+    public var isFanOn: Bool = false
+
+    public var description: String {
+        return "index: \(index), isFanOn: \(isFanOn)"
+    }
 }
 
-public protocol SteamingMode: DeviceMode, TimerOption {}
-public protocol RoastingMode: DeviceMode, TimerOption, TemperatureOption {}
-public protocol ScalingMode: DeviceMode, ScaleOption {}
-public protocol KneadingMode: DeviceMode, TimerOption {}
+public class Heat: Option {
+    public var index: Int = 1
+    public var heatAt: Int = 3
 
-public struct Steaming: SteamingMode {
-    public static var cellIndexes: [Int] = [0, 1]
-    public var hours: Int = 0
-    public var minutes: Int = 10
-    public var seconds: Int = 0
+    public var description: String {
+        return "index: \(index), heatAt: \(heatAt)"
+    }
 }
 
-public struct Device {
-    public let mode: DeviceMode
+public struct DeviceControl {
+    public var options: [Option]
 }
 
-let steaming = Steaming()
-let device = Device(mode: steaming)
-print(device.mode is SteamingMode)
-print(device.mode is RoastingMode)
+let fan = Fan()
+let heat = Heat()
 
-let castedMode = device.mode as? SteamingMode
-print(castedMode?.minutes)
+let deviceControl = DeviceControl(options: [fan, heat])
 
+//print(deviceControl.options.count)
+
+//for option in deviceControl.options {
+//    if option.index == 0 && option is Fan {
+//        (option as! Fan).isFanOn.toggle()
+//    }
+//    if option.index == 1 && option is Heat {
+//        (option as! Heat).heatAt = 6
+//    }
+//}
+
+//print(deviceControl.options.first?.description)
+//print(deviceControl.options.last?.description)
